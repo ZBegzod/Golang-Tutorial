2ace34faafc76413a0b7b4a7c0df13583cea03f4
ZBegod <zohidovbegzod4557@gmail.com>
2023-11-19 19:20:50 +0500

    tutorial lesson

--- lesson5.go
+++ lesson5.go
@@ -1,1 +1,55 @@
 package main
+
+import "fmt"
+
+var a = 4
+var b = 8
+var c = 12
+var number = 458
+
+func main() {
+
+	//if a > b && a > c {
+	//	fmt.Printf("max: %v", a)
+	//} else if b > a && b > c {
+	//	fmt.Printf("max: %v", b)
+	//} else if c > a && c > b {
+	//	fmt.Printf("max: %v", c)
+	//}
+
+	//if number >= 1 && number <= 9 {
+	//	fmt.Print("Bir honali ")
+	//	if number%2 == 0 {
+	//		fmt.Print("Juft son")
+	//	} else {
+	//		fmt.Print("Toq son")
+	//	}
+	//} else if number >= 10 && number <= 99 {
+	//	fmt.Print("Ikki honali ")
+	//	if number%2 == 0 {
+	//		fmt.Print("Juft son")
+	//	} else {
+	//		fmt.Print("Toq son")
+	//	}
+	//} else if number >= 100 && number <= 999 {
+	//	fmt.Print("Uch honali ")
+	//	if number%2 == 0 {
+	//		fmt.Print("Juft son")
+	//	} else {
+	//		fmt.Print("Toq son")
+	//	}
+	//}
+
+	fruits := []string{"apple", "banana", "orange", "liman"}
+	//for i := 0; i < len(fruits); i++ {
+	//	fmt.Println(fruits[i])
+	//}
+	for index, value := range fruits {
+		//fmt.Println(fmt.Sprintf("%v %v", index, value))
+		if index%2 == 0 {
+			fmt.Println("continuing at pos", index)
+			continue
+		}
+		fmt.Printf("the value at index %v is %v \n", index, value)
+	}
+}
--- lesson7.go
+++ lesson7.go
@@ -1,1 +1,59 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+var numberOfList []int
+
+func sayGreeting(n string) {
+	fmt.Printf("Good morning %v \n", n)
+}
+
+func sayBye(n string) {
+	fmt.Printf("Goodbye %v \n", n)
+}
+
+func cycleNames(s []string, f func(string)) {
+	for _, value := range s {
+		f(value)
+	}
+}
+func calculator(symbol string, a int, b int) int {
+	if symbol == "+" {
+		return a + b
+	} else if symbol == "-" {
+		return a - b
+	} else if symbol == "/" {
+		if a != 0 && b != 0 {
+			return a / b
+		}
+	} else if symbol == "*" {
+		return a * b
+	}
+
+	return a
+}
+func getInitialize(n string) (string, string) {
+	s := strings.ToUpper(n)
+	names := strings.Split(s, "")
+	var initials []string
+	for _, value := range names {
+		initials = append(initials, value[:1])
+	}
+	if len(initials) > 1 {
+		return initials[0], initials[1]
+	}
+	return initials[0], "_"
+}
+
+func main() {
+	//cycleNames([]string{"Sardor", "Aziz", "Davron"}, sayGreeting)
+	//result := calculator("*", 5, 6)
+	//fmt.Println(result)
+
+	a, b := getInitialize("Golang Language")
+	fmt.Println(a, b)
+
+}
